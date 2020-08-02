197226cf622a0ec4e1163670aadc7d951955b54e
Luiz Filipe <moresco.luiz@gmail.com>
2020-08-01 23:37:20 -0300

    Implement first version of interview_test.go

---
+++ customerimporter/interview_test.go
@@ -0,0 +1,45 @@
+package customerimporter
+
+import "testing"
+
+func TestSortByDomain(t *testing.T) {
+        domains := map[string]int{
+                "teamwork.com": 4,
+                "bbb.com":      5,
+                "aaa.com":      3,
+        }
+
+        expected := []Domain{
+                {"aaa.com", 3},
+                {"bbb.com", 3},
+                {"teamwork.com", 3},
+        }
+
+        result := sortByDomain(domains)
+
+        for index, item := range result {
+                if domain := expected[index]; domain.name != item.name {
+                        t.FailNow()
+                }
+        }
+
+}
+
+func TestReturnDomainFromEmail(t *testing.T) {
+        domain := "domain.com"
+        email := "my-email@" + domain
+
+        expected := domain
+
+        if result, _ := getDomainFromEmail(email); result != expected {
+                t.FailNow()
+        }
+}
+
+func TestReturnErrorWhenPassInvalidEmailToGetDomainFromEmail(t *testing.T) {
+        email := "invalidemail"
+
+        if _, err := getDomainFromEmail(email); err == nil {
+                t.FailNow()
+        }
+}
